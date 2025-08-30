479fdaafd15246fe3bd6378470c7cd8ba2f58e0f
SheethalShobi <sheethalshobi@gmail.com>
2025-08-30 19:22:54 +0000

    added k8s

--- recommendation-service/app.py
+++ recommendation-service/app.py
@@ -47,4 +47,4 @@ def handle_recommendation_requests():
             return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5005, debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=False)
